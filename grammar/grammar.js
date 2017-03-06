582efaec943e270d1c33fdae621c2b5d10c1014b
Ryan Birmingham <birm@rbirm.us>
2017-03-06 10:10:47 -0500

    add base grammar

--- grammar/grammar.js
+++ grammar/grammar.js
@@ -5,27 +5,53 @@
  */
 
 // we need to add context; we'll do this by switching commands
-var commands = {
-  '': hello,
-  'help':
-  'load *form': loadForm,
-  'open *form': loadForm,
-  'read *response': loadResp,
 
+function home(){
+  var commands = {
+    'hello': hello,
+    'hi': hello,
+    'help':
+    'load *form': loadForm,
+    'open *form': loadForm,
+    'read *response': loadResp
+  };
 }
 
 function loadForm(name){
   // do the form stuff
+  var position  = 0;
+  var form = ""; // get using faudapi
+  var commands = {
+    'reply *reply' : questionReply,
+    'repeat' : questionRepeat
+    'back' : questionBack,
+    'about' : formAbout,
+    'skip' : questionSkip,
+    'home now' : home
+  };
+
 }
 
 function loadResp(name){
-  // fo the response stuff
+  // do the response stuff
+  var position  = 0;
+  var resp = ""; // get using faudapi
+  var commands = {
+    'repeat' : respRepeat
+    'back' : respBack,
+    'next' : respNext,
+    'skip' : respSkip,
+    'parent' : respParent,
+    'home now' : home
+  };
 }
 
 function hello(){
   // just say hi
+  var say = "Hello!"
 }
 
 function help(){
   // just say the help message
+  var say = "We'll help you, just not this early in alpha. Talk to birm."
 }
